5dba48408b12fa7dfc13d000e0c31cc678d7f7f4
fahad-zakir <fahadzakir81@gmail.com>
2020-12-01 15:47:23 -0500

    updated

--- app.js
+++ app.js
@@ -36,12 +36,12 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'))
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public/css/', 'favicon.ico')))
-app.use(favicon(__dirname + 'public/css/favicon.ico'));
+app.use(favicon(path.join(__dirname, 'public/css/', 'favicon.ico')))
+//app.use(favicon(__dirname + 'public/css/favicon.ico'));
 
 // Robots.txt
 app.use('./public/robots.txt', function (req, res, next) {
